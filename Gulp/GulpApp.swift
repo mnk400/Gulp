5817771b6356725459fec18dcada07174d7d2067
Manik Kumar <mnk_400@icloud.com>
2025-12-31 20:31:42 -0800

    Initial Commit

---
+++ Gulp/GulpApp.swift
@@ -0,0 +1,17 @@
+//
+//  GulpApp.swift
+//  Gulp
+//
+//  Created by Manik Kumar on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct GulpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
